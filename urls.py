1dcd2d89647003c5d7f1057f1183456ea17a40d3
Jacob Kaplan-Moss <jacob@jacobian.org>
2011-03-07 17:54:51 -0600

    Finished multi-version support. Also snuck in one-off redirects for fun.

--- urls.py
+++ urls.py
@@ -1,10 +1,37 @@
+from django.conf import settings
 from django.conf.urls.defaults import *
 from django.views.generic import RedirectView
 
+ALL_VERSIONS_RE = '(?P<version>%s)' % '|'.join(settings.DJANGOME['VERSIONS'])
+
 urlpatterns = patterns('',
-    url('^$',               RedirectView.as_view(url='http://docs.djangoproject.com/en/dev/')),
-    url('^t(?P<n>\d+)$',    RedirectView.as_view(url='http://code.djangoproject.com/ticket/%(n)s')),
-    url('^r(?P<n>\d+)$',    RedirectView.as_view(url='http://code.djangoproject.com/changeset/%(n)s')),
-    url('^([\w\-\.]+)$',       'djangome.views.redirect_to_term', name='redirect_to_term'),
-    url('^([\w\-\.]+)/stats$', 'djangome.views.show_term',        name='show_term'),
+    url('^$',
+        RedirectView.as_view(url='http://docs.djangoproject.com/en/dev/')
+    ),
+    url('^t(?P<n>\d+)$',
+        RedirectView.as_view(url='http://code.djangoproject.com/ticket/%(n)s')
+    ),
+    url('^r(?P<n>\d+)$',
+        RedirectView.as_view(url='http://code.djangoproject.com/changeset/%(n)s')
+    ),
+    url('^__oneoffs__$',
+        'djangome.views.manage_oneoffs',
+        name = "manage_oneoffs",
+    ),
+    url('^(?P<term>[\w\-\.]+)$',
+        'djangome.views.redirect_to_term',
+        {'version': settings.DJANGOME['DEFAULT_VERSION']},
+    ),
+    url('^(?P<term>[\w\-\.]+)/stats$',
+        'djangome.views.show_term',
+        {'version': settings.DJANGOME['DEFAULT_VERSION']},
+    ),
+    url('^%s/(?P<term>[\w\-\.]+)$' % ALL_VERSIONS_RE,
+        'djangome.views.redirect_to_term',
+        name = 'redirect_to_term'
+    ),
+    url('^%s/(?P<term>[\w\-\.]+)/stats$' % ALL_VERSIONS_RE,
+        'djangome.views.show_term',
+        name = 'show_term'
+    ),
 )
--- views.py
+++ views.py
@@ -1,27 +1,36 @@
 import redis
 import itertools
 import operator
+import urlparse
 from django import forms
-from django.http import HttpResponse, HttpResponseBadRequest, Http404
+from django import http
 from django.conf import settings
-from django.shortcuts import render_to_response as render
+from django.shortcuts import render
 from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_view_exempt
 
 r = redis.Redis(**settings.REDIS)
 
 class RedirectForm(forms.Form):
-    _prefix = 'http://docs.djangoproject.com/en/dev/'     
-    url = forms.URLField(label='URL', initial=_prefix)
+    _domain = 'docs.djangoproject.com'
+    
+    url = forms.URLField(label='URL', initial='http://%s/' % _domain)
+    title = forms.CharField(label='Title', required=False)
     
     def clean_url(self):
-        url = self.cleaned_data['url']
-        if url.startswith(self._prefix) and url != self._prefix:
-            return url
-        raise forms.ValidationError('Please enter a valid URL starting with %s' % self._prefix)
+        url = urlparse.urlparse(self.cleaned_data['url'])
+        if url.netloc != self._domain:
+            raise forms.ValidationError('Please enter a valid URL starting with http://%s/' % self._domain)
+        return self.cleaned_data['url']
 
-def redirect_to_term(request, term):
+def redirect_to_term(request, version, term):
+    # Check for an admin oneoff first.
+    oneoff_redirect = find_oneoff_redirect(version, term)
+    if oneoff_redirect:
+        return oneoff_redirect
+    
     form = RedirectForm(request.GET or None)
-
+    
     # If we're explicitly choosing a new URL for this term, just go ahead and
     # do that. This could also insert new URLs, so do a brief sanity check to
     # make sure this service can't be used for spam.
@@ -29,11 +38,11 @@ def redirect_to_term(request, term):
         if form.is_valid():
             # Make sure the new URL is in the set of URLs and increment its score.
             url = form.cleaned_data['url']
-            r.sadd('redirects:v1:%s' % term, url)
-            r.incr('redirects:v1:%s:%s' % (term, url))
+            r.sadd('redirects:v2:%s:%s' % (version, term), url)
+            r.incr('redirects:v2:%s:%s:%s' % (version, term, url))
             return redirect(request.GET.get('return_to', url))
     
-    urls = get_urls(term)
+    urls = get_urls(version, term)
     if urls:
         scoregroups = group_urls(urls)
     
@@ -44,7 +53,7 @@ def redirect_to_term(request, term):
         # then issue it.
         if len(winners) == 1:
             url = winners[0]
-            r.incr('redirects:v1:%s:%s' % (term, url))
+            r.incr('redirects:v2:%s:%s:%s' % (version, term, url))
             return redirect(url)
 
         # Otherwise we need to display a list of all choices. We'll present this into
@@ -56,32 +65,105 @@ def redirect_to_term(request, term):
     else:
         winners = losers = None
     
-    return render('choices.html', {
+    return render(request, 'choices.html', {
         'term': term,
         'winners': winners,
         'losers': losers,
-        'form': form
+        'form': form,
+        'version': version,
     })
 
-def show_term(request, term):
-    return render('show.html', {
+def show_term(request, version, term):
+    return render(request, 'show.html', {
         'term': term,
-        'urls': get_urls(term),
+        'version': version,
+        'urls': get_urls(version, term),
         'can_edit': request.COOKIES.get('sekrit') == settings.SECRET_KEY,
     })
-    
-def get_urls(term):
+
+@csrf_view_exempt
+def manage_oneoffs(request):
     """
-    Gets the set of URLs for <term>.
+    Manage the hard-coded one-offs. Admins only.
+    """
+    if request.COOKIES.get('sekrit') != settings.SECRET_KEY:
+        return http.HttpResponseNotAllowed()
+    
+    if request.method == 'POST':
+        if 'action' in request.POST:
+            try:
+                action, version, term, target = (request.POST['action'],
+                                                 request.POST['version'],
+                                                 request.POST['term'],
+                                                 request.POST['target'])
+            except KeyError:
+                return redirect(manage_oneoffs)
+            if action == 'add':
+                add_oneoff(version, term, target)
+            elif action == 'kill':
+                kill_oneoff(version, term)
+        return redirect(manage_oneoffs)
+    
+    keys = r.keys('redirects:v2:oneoffs:*')
+    targets = r.mget(keys) if keys else []
+    
+    oneoffs = []
+    for (k, target) in zip(keys, targets):
+        k = k.replace('redirects:v2:oneoffs:', '')
+        if ':' in k:
+            version, term = k.split(':')
+        else:
+            version, term = None, k
+        oneoffs.append((version, term, target))
+    
+    return render(request, 'oneoffs.html', {
+        'oneoffs': oneoffs,
+    })
+    
+def find_oneoff_redirect(version, term):
+    """
+    Find a one-off redirect for the given version/term.
+    
+    Returns an HttpResponseRedirect if the redirect was found, None otherwise.
+    
+    Increments a count of the found term if successful.
+    """
+    oneoff_keys = ['redirects:v2:oneoffs:%s' % term,
+                   'redirects:v2:oneoffs:%s:%s' % (version, term)]
+    oneoff_redirects = r.mget(oneoff_keys)
+    for k, target in zip(oneoff_keys, oneoff_redirects):
+        if target:
+            r.incr('redirects:v2:%s:%s:%s' % (version, term, target))
+            return redirect(target)
+    return None
+
+def add_oneoff(version, term, target):
+    if version:
+        k = 'redirects:v2:oneoffs:%s:%s' % (version, term)
+    else:
+        k = 'redirects:v2:oneoffs:%s' % term
+    r.set(k, target)
+
+def kill_oneoff(version, term):
+    if version:
+        k = 'redirects:v2:oneoffs:%s:%s' % (version, term)
+    else:
+        k = 'redirects:v2:oneoffs:%s' % term
+    print "kill", k
+    r.delete(k)
+
+def get_urls(version, term):
+    """
+    Gets the set of URLs for <term> in <version>.
     
     Returns a list of (score, url) tuples, sorted by score descending.
     """
     # Sort the set of URLs in redirects:v1:term by the scores (clicks) in
     # redirects:v1:term:url, then get each score along with each URL.
     # This returns a list [score, url, score, url, ...]
-    urls = r.sort('redirects:v1:%s' % term, 
-                  by   = 'redirects:v1:%s:*' % term, 
-                  get  = ('redirects:v1:%s:*' % term, '#'),
+    urls = r.sort('redirects:v2:%s:%s' % (version, term), 
+                  by   = 'redirects:v2:%s:%s:*' % (version, term), 
+                  get  = ('redirects:v2:%s:%s:*' % (version, term), '#'),
                   desc = True)
 
     # Convert that to a list of tuples [(score, url), (score, url), ...]
@@ -95,3 +177,8 @@ def group_urls(urls):
     """
     for (score, group) in itertools.groupby(urls, operator.itemgetter(0)):
         yield (score, [url for score, url in group])
+        
+def firstof(list):
+    for i in list:
+        if i:
+            return i
